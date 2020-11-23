7525efa6ef17d083b04e0a31b06f1b64c31c751a
RSathyanarayanan <sathya.sn176@gmail.com>
2020-11-23 21:23:48 +0530

    Mynewproject

--- README.md
+++
@@ -1,2 +0,0 @@
-# Mynewproject
-Web Development
--- SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  var speakWord = "Good Bye";
-  var bye = {
-    speak: function (name) {
-      console.log(speakWord + " " + name);
-    }
-  };
-  window.bye = bye;
-})(window);
